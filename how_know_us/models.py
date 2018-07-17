a2f4b1b412ea5f7d8b003abe43879e81522da8bd
Javier Colmeiro <javier.colmeiro@impulzia.com>
2018-07-17 16:49:58 +0200

    + add small changes

--- how_know_us/models.py
+++ how_know_us/models.py
@@ -3,12 +3,16 @@
 from openerp import models, fields, api
 
 class how_know_us(models.Model):
+	
     _name = 'how_know_us.how_know_us'
     _rec_name = 'name'
+
     name = fields.Char(string='Caption', size=64, required=True)
 
 
 class AccountAnalyticAccount(models.Model):
+
 	_inherit = 'account.analytic.account'
-	howknowus_id = fields.Many2many('how_know_us.how_know_us', string='How did he know us?', required=True)
+
+	howknowus_ids = fields.Many2many('how_know_us.how_know_us', string='How did he know us?', required=True)
         
