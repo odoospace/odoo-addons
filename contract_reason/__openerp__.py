16d69a31a0b1e326ec480e65ad1a998831185bd1
Javier Colmeiro <javier.colmeiro@impulzia.com>
2018-07-17 17:49:04 +0200

    + add security rule

--- contract_reason/__openerp__.py
+++ contract_reason/__openerp__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'view.xml',
         'res_config_view.xml'
     ],
